ec9510f2979db12295cfc1a57f4cfe018efde644
Luiz Torre <luizmptorre@gmail.com>
2021-09-28 23:55:29 -0300

    Configurando o TypeScript no Bare Workflow

---
+++ src/Components/Welcome.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export function Welcome() {
+    return(
+            <View>
+                <Text> Bare WorkFlow com TypeScript </Text>
+            </View>
+        )
+
+}
