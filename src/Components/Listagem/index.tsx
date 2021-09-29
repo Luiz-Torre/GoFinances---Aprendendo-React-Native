9e1ecdba5c486d67e3eb0c31b853a3a2b494263d
Luiz Torre <luizmptorre@gmail.com>
2021-09-29 20:27:30 -0300

    Construindo a Estrutura da listagem

--- src/Components/Listagem/index.tsx
+++ src/Components/Listagem/index.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import {Container, Header, Title} from './style';
+import {Container, Header, Title, Payment, Footer,Icon, Category, Date } from './style';
 
+interface List{
+    name: string;
+    value: string;
+    date: string;
+    category: 'Vendas' | 'Alimentação' | 'total'
+}
+const icon ={
+    total: 'arrow-up-circle',
+    Alimentação: 'coffe',
+    Vendas: 'dollar-sign'
+}
 
-export function Listagem({} : Card){
+export function Listagem({name, value, date, category} : List){
     return(
         <Container>
             <Header>
-                <Title {name}</Title>
-                <Payment type = {type}> R$ 12.000,00</Payment>
+                <Title >{name}</Title>
+                <Payment category = {category}> {value}</Payment>
             </Header>
 
             <Footer>
-                <Amount type = {type}>{value}</Amount>
-                <LastTransaction type = {type}> {description}</LastTransaction>
+                <Icon name = {icon[category]}/>
+                <Category>{category} </Category>
+                <Date> {date}</Date>
             </Footer>
         </Container>
     )
--- src/Components/Listagem/style.ts
+++ src/Components/Listagem/style.ts
@@ -5,8 +5,25 @@ import styled, { css } from "styled-components/native";
 
 
 interface TypeProps{
-    type: 'up' | 'down' | 'total'
+    category: 'Vendas' | 'Alimentação' | 'total'
 
 }
 
 
+export const Container = styled.View`
+
+    height: ${RFValue(200)}px;
+    width: ${RFValue(300)}px;
+    margin-left: 24px;
+    border-radius: 5px;
+    justify-content: space-between;
+`;
+
+
+export const Header = styled.View``;
+export const Title = styled.Text``;
+export const Payment = styled.Text<TypeProps> ``;
+export const Footer = styled.View``;
+export const Icon = styled(Feather) ``;
+export const Category = styled.Text``;
+export const  Date = styled.Text``;
