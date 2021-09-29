42291111cda0b253753bf8227bc02088276379bb
Luiz Torre <luizmptorre@gmail.com>
2021-09-29 15:50:13 -0300

    Adicionando imagem e tela de boas vindas no Header

--- src/Components/screens/DashBoard/index.tsx
+++ src/Components/screens/DashBoard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Header } from './styles'
+import { Container, Header, Hello, UserName, Photo, User, UserInfo } from './styles'
 
 interface Props{
     title: string;
@@ -8,7 +8,15 @@ interface Props{
 export function Dashboard({title} : Props) {
     return(
             <Container>
-                <Header></Header>
+                <Header>
+                    <UserInfo>
+                        <Photo source={{uri:'https://avatars.githubusercontent.com/u/69131416?v=4'}}/>
+                        <User>
+                            <Hello>Ola,</Hello>
+                            <UserName>Luiz</UserName>
+                        </User>
+                    </UserInfo> 
+                </Header>
             </Container>
         )
 
--- src/Components/screens/DashBoard/styles.ts
+++ src/Components/screens/DashBoard/styles.ts
@@ -14,3 +14,23 @@ export const Header = styled.View`
 
     background-color: ${({theme}) => theme.colors.primary};
 `;
+
+export const UserInfo = styled.View`
+
+`;
+
+export const Hello = styled.Text`
+    font-family: ${({theme}) => theme.fonts.regular};
+`;
+
+export const UserName = styled.Text`
+    font-family: ${({theme}) => theme.fonts.bold};
+`;
+
+
+export const Photo = styled.Image`
+
+`;
+
+export const User = styled.View`
+`;
