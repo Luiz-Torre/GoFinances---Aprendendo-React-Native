31df8d27edc0f8ba00ff68f997879015dcd89a70
Luiz Torre <luizmptorre@gmail.com>
2021-09-29 16:10:09 -0300

    Estilizando parte do nome e imagem do Header

--- src/Components/screens/DashBoard/styles.ts
+++ src/Components/screens/DashBoard/styles.ts
@@ -11,26 +11,36 @@ export const Container = styled.View`
 export const Header = styled.View`
     width: 100%;
     height: ${RFPercentage(39)}px;
-
+    justify-content: center;
+    align-items: center;
     background-color: ${({theme}) => theme.colors.primary};
 `;
 
 export const UserInfo = styled.View`
-
+    flex-direction: row;
+    align-items: center;
 `;
 
 export const Hello = styled.Text`
+    color: ${({theme}) => theme.colors.backgruound};
+    font-size: ${RFValue(18)}px;
+
     font-family: ${({theme}) => theme.fonts.regular};
 `;
 
 export const UserName = styled.Text`
+    color: ${({theme}) => theme.colors.backgruound};
+    font-size: ${RFValue(18)}px;
     font-family: ${({theme}) => theme.fonts.bold};
 `;
 
 
 export const Photo = styled.Image`
-
+    width: ${RFValue(48)}px;
+    height: ${RFValue(48)}px;
+    border-radius: 13px;
 `;
 
 export const User = styled.View`
+    margin-left: 17px;
 `;
